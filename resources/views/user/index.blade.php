832636d8cbbc5993a58f4142a414336c3e62c1e5
LuisGus15 <gustavito1520@gmail.com>
2023-04-29 23:06:54 -0400

    sprint 0

---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,15 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="sm:px-6 lg:px-3">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+               Usuarios
+            </div>
+        </div>
+    </div>
+</x-app-layout>
